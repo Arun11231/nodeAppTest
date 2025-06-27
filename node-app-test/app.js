1c0e284e3d88e1a5f3e6901348305e6aa838c92a
Arun11231 <arungamer20@gmail.com>
2025-06-27 18:01:53 +0000

    fifth commit

--- node-app-test/app.js
+++ node-app-test/app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(' \nThis is a successful auto-deployment!\n');
+  res.end(' \ndisplaying changes for linked in post\n');
 });
 
 const PORT = process.env.PORT || 3000;
