6a61772da8b59479de4b8e7794d218a8b981fe7f
Arun11231 <arungamer20@gmail.com>
2025-06-27 17:52:21 +0000

    fourth attempt

--- node-app-test/app.js
+++ node-app-test/app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hii everyone project third attempt on trying entire set up \n');
+  res.end('Hii everyone project third attempt on trying entire set up \nThis is a successful auto-deployment!\n');
 });
 
 const PORT = process.env.PORT || 3000;
